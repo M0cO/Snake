da0ae9246ff9031619673f93c481853a7cbb4889
CJ Ortega <91446410+M0cO@users.noreply.github.com>
2025-04-29 23:37:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,27 @@ from food import Food
 from countdown import Countdown
 from scoreboard import Score
 import time
+
+def the_game():
+    snake.move()
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score.add_score()
+        snake.add_body_part()
+
+def mode():
+    if difficulty == "easy":
+        time.sleep(EASY)
+        the_game()
+    elif difficulty == "medium":
+        time.sleep(MEDIUM)
+        the_game()
+    elif difficulty == "hard":
+        time.sleep(HARD)
+        the_game()
+
 EASY = .15
-MEDIUM = 10
+MEDIUM = .10
 HARD = .05
 
 screen = Screen()
@@ -28,36 +47,20 @@ screen.onkey(fun=snake.right, key="Right")
 
 game_on = True
 
-def the_game():
-    snake.move()
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        score.add_score()
-        snake.add_body_part()
-
-def mode():
-    if difficulty == "easy":
-        time.sleep(EASY)
-        the_game()
-    elif difficulty == "medium":
-        time.sleep(MEDIUM)
-        the_game()
-    elif difficulty == "hard":
-        time.sleep(HARD)
-        the_game()
-
 while game_on:
     screen.update()
     mode()
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_on = False
         score.game_over()
-        screen.update()
-        time.sleep(1.5)
-        score.try_again()
+
+    for tail_growth in snake.tail_growth[2:]:
+        if snake.head.distance(tail_growth) < 10:
+            game_on = False
+            score.game_over()
+
 
     screen.update()
 
 
 screen.exitonclick()
-
