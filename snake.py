d6f29ecd1558e5f295ed348cba75917db6505725
CJ Ortega <91446410+M0cO@users.noreply.github.com>
2025-04-29 23:38:13 -0700

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,9 +1,12 @@
 from turtle import Turtle
+
 STEPS = 20
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
+START = (40,0),(20,0),(60,0)
+
 
 class Snake:
     def __init__(self):
@@ -12,23 +15,18 @@ class Snake:
         self.head = self.tail_growth[0]
 
     def body(self):
-        for i in range(0, 3):
-            turtle = Turtle(shape="square")
-            turtle.shapesize(stretch_wid=.95,stretch_len=.95)
-            turtle.penup()
-            turtle.color("white")
-            turtle.goto(0 - (i * 20), 0)
-            self.tail_growth.append(turtle)
-    
-    def body_part(self, i):
-        turtle = Turtle(shape="square")
-        turtle.shapesize(stretch_wid=.95, stretch_len=.95)
-        turtle.penup()
-        turtle.color("white")
-        self.tail_growth.append(turtle)
+        for position in START:
+           self.body_part(position)
+
+    def body_part(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.penup()
+        new_segment.color("white")
+        new_segment.goto(position)
+        self.tail_growth.append(new_segment)
 
     def add_body_part(self):
-        self.body_part(self.tail_growth[-1])
+        self.body_part(self.tail_growth[-1].position())
 
     def move(self):
         for tail_num in range(len(self.tail_growth) - 1, 0, -1):
@@ -40,6 +38,7 @@ class Snake:
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
+
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
@@ -51,3 +50,5 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+
+
